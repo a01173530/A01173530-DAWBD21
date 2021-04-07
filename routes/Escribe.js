3e8baa324ce13e40088217bca68571bcb87b9ebc
a01173530 <A01173530@itesm.mx>
2021-04-06 19:02:44 -0500

    Lab 15

--- routes/Escribe.js
+++ routes/Escribe.js
@@ -2,45 +2,15 @@ const file_system=require('fs');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const personasController = require('../controllers/personas_controller');
 
-const personas = [
-        {
-           nombre: 'Mark Otto',
-           correo: 'MarkOtto@hotmail.com',
-           rol: 'Admin'
-        },
-        {
-           nombre: 'Toro',
-           correo: 'Toro@hotmail.com',
-           rol: 'Colaborador'
-        }
-    ];
 
-router.get('/Escribe', (request, response, next) => {
+router.get('/Escribe', personasController.getNuevaPersona);
 
-	response.render('registrar_personal');
+router.post('/Escribe', personasController.postNuevaPersona);
 
-});
 
-router.post('/Escribe', (request, response, next) => {
-   console.log(request.body);
-   personas.push({
-   	       nombre: request.body.write,
-           correo: 'generico@hotmail.com',
-           rol: 'Ninguno'
-
-   });
-   file_system.writeFileSync('Escribe.txt', request.body.write);
-   response.redirect('/Escribe');
-
-   
-});
-
-router.get('/', (request, response, next) => {
-    //response.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
-
-    response.render('index', {personas: personas});
-});
+router.get('/', personasController.get);
 
 
 module.exports = router;
