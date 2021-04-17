68df314bf4c56c0aeb6fa4382a97dd43cae4c49d
a01173530 <A01173530@itesm.mx>
2021-04-17 17:27:18 -0500

    Lab 20

--- routes/Escribe.js
+++ routes/Escribe.js
@@ -3,16 +3,17 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const personasController = require('../controllers/personas_controller');
+const isAuth = require('../util/is-auth');
 
 
-router.get('/Escribe', personasController.getNuevaPersona);
+router.get('/Escribe',  isAuth,personasController.getNuevaPersona);
 
-router.post('/Escribe', personasController.postNuevaPersona);
+router.post('/Escribe',  isAuth, personasController.postNuevaPersona);
 
 
-router.get('/', personasController.get);
+router.get('/',  isAuth, personasController.get);
 
-router.get('/:EspID', personasController.getespecie);
+router.get('/:EspID', isAuth, personasController.getespecie);
 
 
 module.exports = router;
